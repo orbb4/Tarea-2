556dd2c2afcf84dc6111c8a83f3e956bd139354b
orbb4 <reiturra2021@udec.cl>
2022-10-19 14:23:41 -0300

    creado método comprarBebida en clase Expendedor

--- Tarea2/src/tarea2/Expendedor.java
+++ Tarea2/src/tarea2/Expendedor.java
@@ -1,11 +1,16 @@
-
 package tarea2;
 
-public class Expendedor {
+public class Expendedor{
     private DepositoVuelto depositoVuelto;
+    private int precio;
     public Expendedor(int nBebidas, DepositoVuelto depositoVuelto){
         this.depositoVuelto = depositoVuelto;
         
-    
+    }
+    public Bebida comprarBebida(Moneda m, int cual) throws NoHayBebidaException, PagoIncorrectoException{
+        if(m.getValor() < precio){
+            throw new PagoIncorrectoException("Pago insuficiente");
+        //}else if()
+        
     }
 }
