32e2db1519da82aa9c5ead21965874c95708ca0b
Basito0 <76927670+Basito0@users.noreply.github.com>
2022-10-19 13:08:31 -0300

    monedas

--- Tarea2/src/tarea2/Bebida.java
+++ Tarea2/src/tarea2/Bebida.java
@@ -14,3 +14,31 @@ abstract class Bebida{
        
     }
 }
+
+class Moneda{ 
+      
+ } 
+ class Moneda100 extends Moneda{ 
+     private int valor = 100; 
+     public int getValor(){ 
+         return valor; 
+     } 
+ } 
+ class Moneda500 extends Moneda{ 
+     private int valor = 500; 
+     public int getValor(){ 
+         return valor; 
+     } 
+ } 
+ class Moneda1000 extends Moneda{ 
+     private int valor = 1000; 
+     public int getValor(){ 
+         return valor; 
+     } 
+ } 
+ class Moneda1500 extends Moneda{ 
+     private int valor = 1500; 
+     public int getValor(){ 
+         return valor; 
+     } 
+ }
