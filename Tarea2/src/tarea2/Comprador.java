28ca1d737c829daf13bc305af2c55c1973c406e3
cotecita <marcastillo2021@udec.cl>
2022-10-19 15:29:07 -0300

    se corrigen errores por conflicto

--- Tarea2/src/tarea2/Comprador.java
+++ Tarea2/src/tarea2/Comprador.java
@@ -1,14 +1,9 @@
 
 package tarea2;
 
-<<<<<<< HEAD
-
 public class Comprador{
->>>>>>> cd01be670789f145330a6f2013ff31f92d488595
-    
     String tipoBebida;
     int vuelto;
-    
     public Comprador(Moneda m, int b, Expendedor e){
         Bebida bebida = e.comprarBebida(m, b); 
         switch(b){
