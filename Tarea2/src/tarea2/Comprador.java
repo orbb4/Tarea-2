6d0f895c3cab26343592e6172564258749cd2a9d
orbb4 <reiturra2021@udec.cl>
2022-10-19 15:37:51 -0300

    cambios en clase Comprador

--- Tarea2/src/tarea2/Comprador.java
+++ Tarea2/src/tarea2/Comprador.java
@@ -1,15 +1,8 @@
-
 package tarea2;
-
-<<<<<<< HEAD
-
-public class Comprador{
->>>>>>> cd01be670789f145330a6f2013ff31f92d488595
-    
-    String tipoBebida;
-    int vuelto;
-    
-    public Comprador(Moneda m, int b, Expendedor e){
+public class Comprador{    
+    private String tipoBebida;
+    private int vuelto;    
+    public Comprador(Moneda m, int b, Expendedor e) throws NoHayBebidaException, PagoIncorrectoException, PagoInsuficienteException{
         Bebida bebida = e.comprarBebida(m, b); 
         switch(b){
             case 0:
@@ -18,17 +11,20 @@ public class Comprador{
                 tipoBebida = "Sprite";
             case 2:
                 tipoBebida = "Fanta";
-        Moneda moneda_uno = e.getDepositoVuelto();
-        if(moneda_uno == null){vuelto = 0}
-        else{
+        }
+        Moneda moneda_uno = e.getDepositoVuelto().getMoneda();
+        if(moneda_uno == null){
+            vuelto = 0;
+        }else{        
             while(moneda_uno != null){
                 vuelto += moneda_uno.getValor();
-                moneda_uno = e.getDepositoValor();
+                moneda_uno = e.getDepositoVuelto().getMoneda();
                  
             } 
         }
     }
-    public int CuantoVuelto(){
+
+    public int cuantoVuelto(){
         return vuelto;
     }
     public String queBebiste(){
