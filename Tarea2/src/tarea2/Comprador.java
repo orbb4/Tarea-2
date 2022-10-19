e3a663ce98c2d8036b62587b12a164bae233ab0b
cotecita <marcastillo2021@udec.cl>
2022-10-19 14:48:14 -0300

    se realizan cambios en la clase Comprador

--- Tarea2/src/tarea2/Comprador.java
+++ Tarea2/src/tarea2/Comprador.java
@@ -1,28 +1,10 @@
 
 package tarea2;
 
-<<<<<<< HEAD
-public class Comprador {
-=======
-/**
- *
- * @author renat
- */
-
 public class Comprador{
->>>>>>> cd01be670789f145330a6f2013ff31f92d488595
-    
-    Moneda moneda;
-    int tipoBebida;
-    
-    public Comprador(Moneda m, int b, Expendedor e){
-        moneda = m;
-        tipoBebida = b;
-    }
-    public int CuantoVuelto(){
-        
-    }
-    public String queBebiste(){
-        
-    }
+    private Moneda m;
+    public Comprador(Moneda m){
+        this.m = m;
+   
+   }
 }
