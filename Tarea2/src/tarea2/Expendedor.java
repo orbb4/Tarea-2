d6d9e8609debe30413557e24a7e11251d3655779
cotecita <marcastillo2021@udec.cl>
2022-10-19 14:59:36 -0300

    se crean depósitos de bebida en la clase expendedor

--- Tarea2/src/tarea2/Expendedor.java
+++ Tarea2/src/tarea2/Expendedor.java
@@ -3,23 +3,23 @@ import java.util.ArrayList;
 public class Expendedor {
     private DepositoVuelto depositoVuelto;
     private int precio;
-    private ArrayList<CocaCola> bebidasCocaCola = new ArrayList();
-    private ArrayList<Sprite> bebidasSprite = new ArrayList();
-    private ArrayList<Fanta> bebidasFanta = new ArrayList();
+    private Deposito depCocaCola = new Deposito();
+    private Deposito depSprite = new Deposito();
+    private Deposito depFanta = new Deposito();
     public Expendedor(int nBebidas, DepositoVuelto depositoVuelto, int precio){
         this.depositoVuelto = depositoVuelto;
         this.precio = precio;
         CocaCola bebCoca = new CocaCola(32883);
         for(int i=0; i<nBebidas; ++i){
-            bebidasCocaCola.add(bebCoca);
+            depCocaCola.addBebida(bebCoca);
         }
         Sprite bebSprite = new Sprite(29798);
         for(int i=0; i<nBebidas; ++i){
-            bebidasSprite.add(bebSprite);
+            depSprite.addBebida(bebSprite);
         }
         Fanta bebFanta = new Fanta(39845);
         for(int i=0; i<nBebidas; ++i){
-            bebidasFanta.add(bebFanta);
+            depFanta.addBebida(bebFanta);
         }
                
     }
